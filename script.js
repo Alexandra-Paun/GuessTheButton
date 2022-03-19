7ea8a3c27e35c8d5e23b7dfea507290475ed0103
Alexandra-Paun <101888869+Alexandra-Paun@users.noreply.github.com>
2022-03-19 11:26:13 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function fun(idButton) {
-    if (Math.floor((Math.random() * 3) + 1) == idButton) {
-        form.textContent = "YOU WIN"
-    } else {
-        form.textContent = "YOU LOST"
-    }
-}
-
